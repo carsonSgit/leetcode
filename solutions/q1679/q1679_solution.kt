2e1440d96ad5b04d5d5f6cdb399fb5cbaf7d96ff
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-07-02 19:13:53 -0400

    Kotlin Optimize q1679_solution.kt

--- solutions/q1679/q1679_solution.kt
+++ solutions/q1679/q1679_solution.kt
@@ -1,25 +1,23 @@
 class Solution {
     fun maxOperations(nums: IntArray, k: Int): Int {
         nums.sort()
-
         var left = 0
         var right = nums.size - 1
         var operations = 0
 
-        while (left < right){
-            var sum = nums[left] + nums[right]
-            if(sum == k){
-                operations++
-                left++
-                right--
-            }
-            else if(sum > k){
-                right--
-            }
-            else{
-                left++
+        while (left < right) {
+            val sum = nums[left] + nums[right]
+            when {
+                sum == k -> {
+                    operations++
+                    left++
+                    right--
+                }
+                sum > k -> right--
+                else -> left++
             }
         }
+
         return operations
     }
 }
