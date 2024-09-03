6733661a83c9ff0f827b30831b338fe48ed02160
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-09-03 14:59:11 -0400

    Add q1768_solution.kt

---
+++ solutions/q1768/q1768_solution.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        return buildString {
+            val minLength = minOf(word1.length, word2.length)
+
+            for (i in 0..minLength - 1) {
+                append(word1[i])
+                append(word2[i])
+            }
+
+            append(word1.substring(minLength))
+            append(word2.substring(minLength))
+        }
+    }
+}
